82bc48f5232e8a7204fead9528329ca5c5590258
ifares <hmsteven37@gmail.com>
2025-07-28 16:39:49 +0100

    explication

--- framework/vdom.js
+++ framework/vdom.js
@@ -1,3 +1,23 @@
+// Vnode class used  construct the node (render it)
+// it accept 3 params in the constructor the tag name as str-> an object of attributs , and array of children 
+// attrbuts stored in obeject since they have a key and value not like children need just value such as tags and texts
+// it has  render method to current elemnt to real DOM element  
+// thing about as receipe ,, p , h1 , div , class , receipe -> converted into object via the constructor ,, then render comes to cook it ,, and give us the real DOM
+
+// explication of render method
+// this -> belong to the current object , so this.tag means current tag
+//  step -> 1 create the element by its name -> <div> </div>
+//  step -> 2  iterate over the attrbs objects ,, using object.Entries to bring key and value 
+// if the attrbuts  name start with on such as on-click on-input -> and the type of value is afunction -> attach an event listner to it 
+// example ->  onClick: () => alert('clicked'), ,, slice ,, to make on click cliclk 
+// if the tag is type input and has attrbut value ,, means attach the elemnt.value <= value
+// if the tag is type input and has attrbut checkbox .. gives the checked value -> el.checked = Boolean(value); true of false depends 
+// special case like onClick, value, or checked
+// the else is just normal attrbs like placeholder  etc
+// step -> 3 iterate over the children tag 
+// if the child is null or undef ,, means no child ,, just skip 
+// if the child is text  (string) => create text node and append it to the elemnt
+// if child is node ,, appned it and recall the render func
 class VNode {
   constructor(tag, attrs = {}, children = []) {
     this.tag = tag.toLowerCase();
